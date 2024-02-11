1aee937ad9d37e260b24f23539dbdc89cb34ad81
rim <rimdagdoug99@gmail.com>
2024-02-11 15:15:48 +0100

    MVC (Model-View-Controller)

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,25 +1,11 @@
-const express = require('express')
-const router = express.Router()
-var moment = require('moment');
-const User= require("../models/customerSchema");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/customerSchema");
+const userController = require("../controllers/userController");
+var moment = require("moment");
 
+router.get("", userController.user_add_get);
+// POST Requst
+router.post("", userController.user_post);
 
-
-
-
-router.get("", (req, res) => {
-    res.render("user/add")
-  });
-
-router.post("", (req, res) => {
-   
-    
-    User.create(req.body).then(() => {
-      res.redirect("/");
-    }).catch((err) => {
-      console.log(err);
-    });
-   
-  });
-
-  module.exports = router
+module.exports = router;
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,92 +1,22 @@
-const express = require('express')
-const router = express.Router()
-var moment = require('moment');
-const User= require("../models/customerSchema");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/customerSchema");
+var moment = require("moment");
+const userController = require("../controllers/userController");
+// GET Requst
 
-//get
+router.get("/", userController.user_index_get);
 
+router.get("/edit/:id", userController.user_edit_get);
 
-router.get("/", (req, res) => {
-  
-    User.find().then((result) => {
-      
-      res.render("index", {arr: result , moment: moment});
+router.get("/view/:id", userController.user_view_get);
 
-    }).catch((err) => {
-      console.log(err)
-    })
-    
-      
-    });
-  
+router.post("/search", userController.user_search_post);
 
+// DELETE Request
+router.delete("/edit/:id", userController.user_delete);
 
+// PUT Requst
+router.put("/edit/:id", userController.user_put);
 
-
- // app.get("/user/view.html", (req, res) => {
-    //res.render("user/view")
-  //});
-
-router.get("/edit/:id", (req, res) => {
-    User.findById(req.params.id).then((result) => {
-      res.render("user/edit", {obj: result, moment: moment})
-    }).catch((err) => {
-      console.log(err)
-    })
-    
-  });
-
-router.get("/view/:id", (req, res) => {
-    // result ==> object
-    User.findById(req.params.id)
-      .then((result) => {
-        res.render("user/view", { obj: result, moment: moment });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-
-  //post
-
-
-
-router.post("/search", (req, res) => {
-   
-    console.log("------------------------")
-    const searchText = req.body.searchText.trim();
-    User.find({ $or: [{firstName: searchText}, {lastName: searchText}]}).then((result) => {
-     console.log(result)
-      res.render("user/search", {arr: result , moment: moment})
-    }).catch((err) => {
-      console.log(err);
-    });
-   
-  });
-
-
-  // delete request
-router.delete("/edit/:id", (req, res) => {
-
-    User.findByIdAndDelete(req.params.id).then(() => {
-      res.redirect("/");
-
-    }).catch((err) => {
-      console.log(err)
-    })
-
-  });
-// put request
-router.put("/edit/:id" , (req,res) => {
-    
-    User.findByIdAndUpdate(req.params.id, req.body).then((result) => {
-      
-      res.redirect("/")
-    }).catch((err) => {
-      console.log(err)
-    })
-  });
-
-
-module.exports = router
+module.exports = router;
